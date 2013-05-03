34595f47582ed6ae38ffe53aadc3245c555244d2
David Hauweele <david@hauweele.net>
2013-05-03 14:38:43 +0200

    Add the upload script.
    This script allows to quickly upload files on a server with rsync.

---
+++ bin/upload.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+UP_URL="bilbo.hauweele.net"
+UP_PATH="public_html/upload"
+DOWN_URL="http://www.hauweele.net/~gawen/upload"
+BASE="abcdefghijklmnopqrstuvwxyz0123456789"
+
+if [ $# != 1 ]
+then
+  echo "usage: $0 FILE"
+  exit 1
+fi
+
+file=$1
+
+if [ ! -r "$file" ]
+then
+  echo "cannot read $file"
+  exit 1
+fi
+
+# This is not 100% percent correct.
+# But it should avoid collision in
+# most cases.
+crc=$(crc32 "$file" | cut -d' ' -f1 | base -O "$BASE")
+size=$(sizeof "$file" | cut -d':' -f2 | base -O "$BASE")
+upname=${size}${crc}
+
+rsync --progress --chmod=a+r "$file" $UP_URL:$UP_PATH/$upname
+
+echo ""
+echo "Uploaded:"
+echo "$DOWN_URL/$upname"
