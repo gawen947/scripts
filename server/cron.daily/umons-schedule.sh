08799d46c7baf7d31e5f567087d62e0b6aecb077
David Hauweele <david@hauweele.net>
2013-06-06 23:06:09 +0200

    umons-schedule: Add the script umons schedule
    This script is used to clean the ICS file provided with the new calendar
    application we use at UMONS.

---
+++ server/cron.daily/umons-schedule.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+. /etc/umons-schedule.conf
+
+while read line
+do
+  url=$(echo $line | cut -d' ' -f 1)
+  out=$(echo $line | cut -d' ' -f 2)
+
+  tmp=$(tempfile)
+  wget "$url" -O $tmp > /dev/null 2>&1
+
+  # Clean the file
+  timezone="Europe\\/Brussels"
+  asciify $tmp     > $tmp.0
+  dos2unix $tmp.0  > /dev/null 2>&1
+  cat $tmp.0 | sed "s/DTSTART:\\([[:digit:]]*T[[:digit:]]*\\)$/DTSTART;TZID=$timezone:\\1/" > $tmp
+  cat $tmp   | sed "s/DTEND:\\([[:digit:]]*T[[:digit:]]*\\)$/DTEND;TZID=$timezone:\\1/" > $tmp.0
+
+  cp $tmp.0 $out
+  chown $OWNING_USER:www-data $out
+  chmod a+r $out
+
+  rm $tmp $tmp.0
+done < /etc/umons-schedule
+
+exit 0
