62de908d146372b20c1b0c799bf0fe5178f0679c
David Hauweele <david@hauweele.net>
2013-06-06 23:04:19 +0200

    upload: Add the server side script
    With the clean function, the upload script now comes in two parts. First
    the client side and second the server side which resides on the server
    and clean the uploaded files when needed.

---
+++ server/cron.daily/clean-uploaded.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Copyright (c) 2013 David Hauweele <david@hauweele.net>
+
+now=$(date +"%s")
+
+for user in $(ls /home)
+do
+    home=/home/$user
+    ulf=$home/.uploaded-limit
+    if [ ! -r $ulf ]
+    then
+        continue
+    fi
+
+    tmp=$(tempfile)
+    while read line
+    do
+        file=$(echo $line | cut -d' ' -f 1)
+        limit=$(echo $line | cut -d' ' -f 2)
+
+        if [ "$now" -gt "$limit" ]
+        then
+            rm -f $home/public_html/upload/$file
+        else
+            echo "$line" >> $tmp
+        fi
+    done < $ulf
+    mv $tmp $ulf
+    chown $user:$user $ulf
+done
+
