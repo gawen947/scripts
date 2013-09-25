bbee1c3e655753a158bd5f315a0f9ea73946698a
David Hauweele <david@hauweele.net>
2013-09-25 20:30:00 +0900

    gpg-crypt: Add the gpg-crypt/gpg-decrypt scripts
    These two scripts allow to quickly cipher a message with asymmetric
    ciphering. It will ask for options (private-key, public-key, message)
    through an editor like vim and display the result in this editor.

---
+++ bin/gpg-crypt.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+sendf=$(tempfile)
+destf=$(tempfile)
+messf=$(tempfile)
+temp=$(tempfile)
+
+echo "$GPG_DEFAULT_KEY" >> $sendf
+echo "#################################" >> $sendf
+echo "# Secret key to sign with.      #" >> $sendf
+echo "#                               #" >> $sendf
+echo "# This can be either a name or  #" >> $sendf
+echo "# an email address.             #" >> $sendf
+echo "#################################" >> $sendf
+
+echo "" >> $destf
+echo "#################################" >> $destf
+echo "# Public key to send to.        #" >> $destf
+echo "#                               #" >> $destf
+echo "# This can be either a name or  #" >> $destf
+echo "# an email address.             #" >> $destf
+echo "#################################" >> $destf
+
+echo "" >> $messf
+echo "#################################" >> $messf
+echo "# Enter your message here...    #" >> $messf
+echo "#################################" >> $messf
+
+vim $sendf
+vim $destf
+vim $messf
+
+send="$(grep . $sendf | grep -v '^#')"
+dest="$(grep . $destf | grep -v '^#')"
+cat $messf | grep -v "^#" > $temp
+cp $temp $messf; rm $temp
+
+if [ -n "$send" -a -n "$dest" -a -s $messf ]
+then
+  gpg -esa -u "$send" -r "$dest" $messf
+  vim $messf.asc
+  rm $messf.asc
+else
+  echo "Private key / Destination key / Message not specified."
+  echo "Nothing encrypted."
+fi
+
+rm $messf
+rm $sendf
+rm $destf
---
+++ bin/gpg-decrypt.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+messf=$(tempfile)
+outpf=$(tempfile)
+tempf=$(tempfile)
+echo "" >> $messf
+echo "###################################" >> $messf
+echo "# Enter the received message here #" >> $messf
+echo "###################################" >> $messf
+
+vim $messf
+cat $messf | grep -v "^#" > $tempf
+cp $tempf $messf; rm $tempf
+
+if [ -s $messf ]
+then
+  gpg -d $messf > $outpf
+  read null
+  vim $outpf
+else
+  echo "Private key / Destination key / Message not specified."
+  echo "Nothing encrypted."
+fi
+
+rm $messf
+rm $outpf
