3ec14c62323970662904efbe57c98a737082b9b6
David Hauweele <david@hauweele.net>
2013-09-25 21:12:30 +0900

    qrcode-wifi: Add the qrcode-wifi script
    This script allows to create a qrcode which contains the connection
    information for a WiFi network. It will request for the necessary
    informations and create the qrcode. It will then display it with Feh
    for scanning or saving into another file.

---
+++ bin/qrcode-wifi.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+#
+# Copyright (c) 2013 David Hauweele <david@hauweele.net>
+
+if ! which qrencode > /dev/null
+then
+  echo "error: requires qrencode"
+  exit 1
+fi
+
+if ! which feh > /dev/null
+then
+  echo "error: requires feh"
+  exit 1
+fi
+
+authf=$(tempfile)
+ssidf=$(tempfile)
+pskf=$(tempfile)
+hiddenf=$(tempfile)
+
+echo "nopass" >> $authf
+echo "############################" >> $authf
+echo "# Authentification method. #" >> $authf
+echo "#                          #" >> $authf
+echo "# WEP, WPA, nopass         #" >> $authf
+echo "############################" >> $authf
+
+echo "" >> $ssidf
+echo "############################" >> $ssidf
+echo "# SSID name.               #" >> $ssidf
+echo "############################" >> $ssidf
+
+echo "" >> $pskf
+echo "############################" >> $pskf
+echo "# Preshared key.           #" >> $pskf
+echo "############################" >> $pskf
+
+echo "false" >> $hiddenf
+echo "############################" >> $hiddenf
+echo "# Hidden SSID.             #" >> $hiddenf
+echo "############################" >> $hiddenf
+
+vim $authf
+vim $ssidf
+auth="$(grep . $authf | grep -v '^#')"
+
+if [ -z "$auth" ]
+then
+  auth="nopass"
+fi
+
+if [ "$auth" != "nopass" ]
+then
+  vim $pskf
+fi
+
+vim $hiddenf
+
+
+ssid="$(grep . $ssidf | grep -v '^#')"
+psk="$(grep . $pskf | grep -v '^#')"
+hidden="$(grep . $hiddenf | grep -v '^#')"
+
+rm $pskf
+rm $ssidf
+rm $hiddenf
+rm $authf
+
+if [ "$hidden" = "true" ]
+then
+  hidden="H:true"
+else
+  hidden=""
+fi
+
+qrcodef=$(tempfile)
+qrencode -s 10 -t png -o $qrcodef "WIFI:T:$auth;S:$ssid;P:$psk;$hidden;"
+feh $qrcodef
+rm $qrcodef
