6b36e7d90018c423a7f8bf5f3f3c5707991a4e24
David Hauweele <david@hauweele.net>
2014-01-09 19:41:21 +0100

    clean-uploaded: Fix identation
    We now use two spaces indentation instead of four spaces.

--- server/cron.daily/clean-uploaded.sh
+++ server/cron.daily/clean-uploaded.sh
@@ -5,27 +5,27 @@ now=$(date +"%s")
 
 for user in $(ls /home)
 do
-    home=/home/$user
-    ulf=$home/.uploaded-limit
-    if [ ! -r $ulf ]
+  home=/home/$user
+  ulf=$home/.uploaded-limit
+  if [ ! -r $ulf ]
+  then
+    continue
+  fi
+
+  tmp=$(tempfile)
+  while read line
+  do
+    file=$(echo $line | cut -d' ' -f 1)
+    limit=$(echo $line | cut -d' ' -f 2)
+
+    if [ "$now" -gt "$limit" ]
     then
-        continue
+      rm -f $home/public_html/upload/$file
+    else
+      echo "$line" >> $tmp
     fi
-
-    tmp=$(tempfile)
-    while read line
-    do
-        file=$(echo $line | cut -d' ' -f 1)
-        limit=$(echo $line | cut -d' ' -f 2)
-
-        if [ "$now" -gt "$limit" ]
-        then
-            rm -f $home/public_html/upload/$file
-        else
-            echo "$line" >> $tmp
-        fi
-    done < $ulf
-    mv $tmp $ulf
-    chown $user:$user $ulf
+  done < $ulf
+  mv $tmp $ulf
+  chown $user:$user $ulf
 done
 
