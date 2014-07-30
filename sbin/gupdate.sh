115f63946d9fd0037a2377b10d4a078c3c1ae6ef
David Hauweele <david@hauweele.net>
2014-07-30 03:54:16 +0200

    gupdate: Add the script gupdate
    This script detects the current OS/distribution and updates the system
    accordingly.

---
+++ sbin/gupdate.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Common options:
+CLEAN=false
+
+# FreeBSD options:
+PORTMASTER_EXTRA="--delete-build-only -P"
+
+# Detect the update method
+os=$(uname -s)
+if [ -r /etc/debian_version ]
+then
+  method=apt
+elif [ "$os" = "FreeBSD" ]
+then
+  method=pkgng_portmaster
+  case "$1" in
+    port)   method=portmaster;;
+    pkg)    method=pkgng;;
+    "")     method=pkgng;;
+    *)
+      echo "Specify port or pkg (default: pkg)."
+      exit 1
+      ;;
+  esac
+fi
+
+case "$method" in
+  apt)
+    apt-get update
+    apt-get upgrade
+    $CLEAN && apt-get clean
+    ;;
+  pkgng)
+    pkg update
+    pkg upgrade
+    $CLEAN && pkg clean
+    ;;
+  portmaster)
+    _pwd=$(pwd)
+    cd /usr/ports
+    make update
+    portmaster -a $PORTMASTER_EXTRA
+    cd "$_pwd"
+    ;;
+  pkgng_portmaster)
+    $0 pkg
+    $0 portmaster
+    ;;
+  *)
+    echo "Invalid method!"
+    exit 1
+    ;;
+esac
+
+exit 0
