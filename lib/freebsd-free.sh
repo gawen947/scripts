a3e08848d2fb5ac3e5c7fab89f88d866739c4a84
David Hauweele <david@hauweele.net>
2014-11-14 17:17:50 +0100

    freebsd-free: Linux support.

--- lib/freebsd-free.sh
+++ lib/freebsd-free.sh
@@ -8,35 +8,60 @@ UNIT="MB"
 # Ratio Active/Inactive that gives an activity of 100%
 ACTIVITY_EQUIV_RATIO=6
 
-if [ "$(uname -s)" != "FreeBSD" ]
-then
-  echo "support FreeBSD only."
-  exit 1
-fi
-
 pct() (printf "%3.2f%%" $(rpnc "$1" "$2" / 100 .))
 to_unit() (printf "%.0f" $(rpnc "$1" "$FACTOR" / | cut -d'.' -f1))
-get_sysctl() (sysctl "$1" | cut -d':' -f2 | sed 's/ *//g')
-get_mem() (
-  page_count=$(get_sysctl "vm.stats.vm.v_${1}_count")
-  rpnc "$page_count" "$page_size" .
-)
 
 # Retrieve memory information
-page_size=$(get_sysctl hw.pagesize)
+case "$(uname -s)" in
+  FreeBSD)
+    get_sysctl() (sysctl "$1" | cut -d':' -f2 | sed 's/ *//g')
+    get_mem() (
+      page_count=$(get_sysctl "vm.stats.vm.v_${1}_count")
+      rpnc "$page_count" "$page_size" .
+    )
 
-mem_all=$(get_mem page)
-mem_free=$(get_mem free)
-mem_wire=$(get_mem wire)
-mem_active=$(get_mem active)
-mem_inactive=$(get_mem inactive)
-mem_cache=$(get_mem cache)
+    page_size=$(get_sysctl hw.pagesize)
 
-swap_info=$(swapinfo -k | grep "/dev/" | awk '{ used+=$3; all+=$2; } END { print used, all ; }')
-swap_used=$(echo "$swap_info" | cut -d' ' -f1)
-swap_used=$(rpnc "$swap_used" 1024 .)
-swap_all=$(echo "$swap_info" | cut -d' ' -f2)
-swap_all=$(rpnc "$swap_all" 1024 .)
+    mem_all=$(get_mem page)
+    mem_free=$(get_mem free)
+    mem_wire=$(get_mem wire)
+    mem_active=$(get_mem active)
+    mem_inactive=$(get_mem inactive)
+    mem_cache=$(get_mem cache)
+
+    swap_info=$(swapinfo -k | grep "/dev/" | awk '{ used+=$3; all+=$2; } END { print used, all ; }')
+    swap_used=$(echo "$swap_info" | cut -d' ' -f1)
+    swap_used=$(rpnc "$swap_used" 1024 .)
+    swap_all=$(echo "$swap_info" | cut -d' ' -f2)
+    swap_all=$(rpnc "$swap_all" 1024 .)
+
+    wire_name="Wire    :"
+    ;;
+  Linux)
+    get_mem() (
+      kb=$(cat /proc/meminfo | grep "^${1}:" | grep -oE "[[:digit:]]+ kB" | sed 's/ kB//g')
+      rpnc "$kb" 1024 .
+    )
+
+    mem_all=$(get_mem MemTotal)
+    mem_free=$(get_mem MemFree)
+    mem_wire=$(get_mem Buffers)
+    mem_inactive=$(get_mem Inactive)
+    mem_active=$(get_mem Active)
+    mem_cache=$(get_mem Cached)
+
+    swap_all=$(get_mem SwapTotal)
+    swap_free=$(get_mem SwapFree)
+    swap_used=$(rpnc "$swap_total" "$swap_free" -)
+
+
+    wire_name="Buffers :"
+    ;;
+  *)
+    echo "$(uname -s) is not supported yet."
+    exit 1
+    ;;
+esac
 
 # Compute details
 mem_used=$(rpnc "$mem_active" "$mem_inactive" +)
@@ -68,5 +93,5 @@ echo "Details:"
 echo "  Active  : $(to_unit $mem_active) $UNIT ($(pct $mem_active $mem_all))"
 echo "  Inactive: $(to_unit $mem_inactive) $UNIT ($(pct $mem_inactive $mem_all))"
 echo "  Cache   : $(to_unit $mem_cache) $UNIT ($(pct $mem_cache $mem_all))"
-echo "  Wire    : $(to_unit $mem_wire) $UNIT ($(pct $mem_wire $mem_all))"
+echo "  $wire_name $(to_unit $mem_wire) $UNIT ($(pct $mem_wire $mem_all))"
 echo "  Total   : $(to_unit $mem_all) $UNIT"
