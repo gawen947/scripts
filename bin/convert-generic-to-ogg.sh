b662125882c8623c916cf2c0f783ec51e900c520
David Hauweele <david@hauweele.net>
2015-11-05 14:24:32 +0100

    convert-generic-to-ogg: Multiple fixes and clamp
    - Fix a file removed two times resulting in no such file or directory.
    - Add a clamp feature to limit the encoding bitrate.
    - Add AAC to supported formats.

--- bin/convert-generic-to-ogg.sh
+++ bin/convert-generic-to-ogg.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-if [ "$#" != 1 ]
-then
-  echo "usage: $0 [input-file]"
-  exit 1
-fi
-
-original_path="$1"
+clamp_rate=9999
+case "$#" in
+  1)
+    original_path="$1"
+    ;;
+  2)
+    clamp_rate="$1"
+    original_path="$2"
+    ;;
+  *)
+    echo "usage: $0 [[clamp-rate] input-file]"
+    exit 1
+    ;;
+esac
 
 # Parse extension and recompose new path with correct extension.
-original_extension=$(echo "$1"|awk -F . '{ print $NF }')
+original_extension=$(echo "$original_path"|awk -F . '{ print $NF }')
 new_path=$(dirname "$original_path")/$(basename "$original_path" ."$original_extension").ogg
 
 
@@ -33,7 +40,7 @@ rm "$decoded"
 
 # Skip unsupported file formats.
 found=""
-for supported_extension in ogg flac mp3 wma oga aiff riff wav au mpc m4a
+for supported_extension in ogg flac mp3 wma oga aiff riff wav au mpc m4a aac
 do
   if [ "$normalized_extension" = "$supported_extension" ]
   then
@@ -49,20 +56,22 @@ fi
 echo "Decoding..."
 case "$normalized_extension" in
   mp3)
-    mpg321 -w - -- "$original_path" > "$decoded";;
+    #mpg321 -w - -- "$original_path" > "$decoded";;
+    decoded="${decoded}.wav"
+    ffmpeg -i "$original_path" "$decoded";;
   ogg)
     echo "will not re-encode in the same format."
     exit 0
     ;;
   flac)
-    rate="999"
+    rate="9999"
     flac -d "$original_path" -o "$decoded";;
   *)
     # Fallback to ffmpeg.
-    rm "$decoded"
     decoded="${decoded}.wav"
     ffmpeg -i "$original_path" "$decoded";;
 esac
+echo
 
 # Sometimes we cannot decode the rate
 if [ -z "$rate" ]
@@ -70,8 +79,14 @@ then
   rate="128"
 fi
 
+if [ "$rate" -ge "$clamp_rate" ]
+then
+  echo "clamp from ${rate}Kbps to ${clamp_rate}Kbps"
+  rate="$clamp_rate"
+fi
+
 # Rate to quality
-for equiv_bitrate in 192q6 160q5 128q4 112q3 96q2 80q1 64q0 45q-1 32q-2
+for equiv_bitrate in 500q10 320q9 256q8 224q7 192q6 160q5 128q4 112q3 96q2 80q1 64q0 45q-1 32q-2
 do
   base_rate=$(echo "$equiv_bitrate" | cut -d'q' -f 1)
 
@@ -83,7 +98,6 @@ do
 done
 
 # Some stats
-echo
 echo "Encode from $normalized_extension ${rate}Kbps to Vorbis q$quality ~${base_rate}Kbps"
 echo
 
