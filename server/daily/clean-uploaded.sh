f4c8b99a3578d1645ea829bd1fd72fcedd701532
David Hauweele <david@hauweele.net>
2017-02-01 00:27:36 +0100

    clean-uploaded: Use more descriptive variables.

--- server/daily/clean-uploaded.sh
+++ server/daily/clean-uploaded.sh
@@ -6,15 +6,15 @@ now=$(date +"%s")
 for user in $(ls /home)
 do
   home=/home/$user
-  ulf=$home/.uploaded-limit
-  umf=$home/.uploaded-map
-  if [ ! -r $ulf -o ! -r $umf ]
+  limit_file=$home/.uploaded-limit
+  map_file=$home/.uploaded-map
+  if [ ! -r $limit_file -o ! -r $map_file ]
   then
     continue
   fi
 
-  tmp_ulf=$(mktemp)
-  tmp_umf=$(mktemp)
+  tmp_limit=$(mktemp)
+  tmp_map=$(mktemp)
   while read line
   do
     file=$(echo $line | cut -d' ' -f 1)
@@ -24,19 +24,18 @@ do
     then
       rm -f $home/public_html/upload/$file
     else
-      map=$(cat "$umf" | grep "^$file ")
+      map=$(cat "$map_file" | grep "^$file ")
       if [ -n "$map" ]
       then
-        echo "$map" >> $tmp_umf
+        echo "$map" >> $tmp_map
       fi
-      echo "$line" >> $tmp_ulf
+      echo "$line" >> $tmp_limit
     fi
-  done < $ulf
-  mv $tmp_ulf $ulf
-  mv $tmp_umf $umf
-  chown $user:$user $ulf
-  chown $user:$user $umf
-  chmod 644 $umf
-done
+  done < $limit_file
 
-# TODO: check files without limit
+  mv $tmp_limit $limit_file
+  mv $tmp_map   $map_file
+  chown $user:$user $limit_file
+  chown $user:$user $map_file
+  chmod 644 $map_file
+done
