f382530935cbc25fc9195cb461f67b30a540131a
David Hauweele <david@hauweele.net>
2018-06-09 18:58:30 +0200

    wp-random: Reverse directory and command.

--- lib/wp-random.sh
+++ lib/wp-random.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-if [ $# != 3 ]
+if [ $# -lt 3 ]
 then
-  echo "usage: $(basename $0) sleep-duration command directory"
+  echo "usage: $(basename $0) sleep-duration directory command [command-args ...]"
   exit 1
 fi
 
 sleep_duration="$1"
-command_exec="$2"
-directory="$3"
+directory="$2"
+shift; shift
 
 while true
 do
   wp=$(find "$directory" -type f | sort -R | head -1)
-  echo $command_exec "$wp"
-  $command_exec "$wp"
+  echo $* "$wp"
+  $* "$wp"
   sleep "$sleep_duration"
 done
